2ec65f6be5115468eeb01eb108491e95d967f0da
CCF2P <yura.smolyaninov.01@mail.ru>
2023-10-07 15:39:56 +1000

    Сделать округление и обрезку

--- Lab_1_c_4/IDZ/main.py
+++ Lab_1_c_4/IDZ/main.py
@@ -1,5 +1,14 @@
 import sqlite3
 
+'''
+con = sqlite3.connect("storeDB.db")
+f_damp = open("store.db", "r", encoding="utf-8-sig")
+damp = f_damp.read()
+f_damp.close()
+con.executescript(damp)
+con.commit()
+'''
+
 con = sqlite3.connect("./storeDB.db")
 
 cursor = con.cursor()
@@ -59,9 +68,38 @@ for row in res:
 print()
 
 print("Задание 4")
+cursor.execute('''
+UPDATE book
+SET price = price * 1.1
+WHERE 2 <= (SELECT COUNT(buy_book.book_id)
+				FROM buy_book
+				GROUP BY buy_book.book_id
+                HAVING buy_book.book_id = book.book_id);
+''')
+cursor.execute("SELECT * FROM book;")
+res = cursor.fetchall()
+print([description[0] for description in cursor.description])
+for row in res:
+    print(row)
+print()
 
-
-
+print("Задание 5")
+cursor.execute('''
+SELECT row_number() OVER win AS "№",
+       "Достоевский Ф.М." AS "Автор",
+	   book.title AS "Книга",
+       book.amount AS "Кол-во",
+       rank() OVER win AS "Ранг",
+       cume_dist() OVER win AS "Распределение",
+       percent_rank() OVER win AS "Ранг%"
+FROM author, book
+WHERE author.author_id = book.author_id AND author.name_author = "Достоевский Ф.М."
+WINDOW win AS (ORDER BY book.amount);
+''')
+res = cursor.fetchall()
+print([description[0] for description in cursor.description])
+for row in res:
+    print(row)
 print()
 
 con.close()
