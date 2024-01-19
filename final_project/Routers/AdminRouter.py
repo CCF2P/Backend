98ee9f5ad7297bdbe71a8043cc26da2d1a9f5aa4
CCF2P <yura.smolyaninov.01@mail.ru>
2024-01-19 17:45:08 +1000

    Поправил отчте и проект

--- final_project/Routers/AdminRouter.py
+++ final_project/Routers/AdminRouter.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Depends
 
 from sqlalchemy.orm import Session
 
-from Database.createDatabase import *
-from Database.Schemas.schemas import *
-from Models.Models import *
+from Database.createDatabase import engine, SessionLocal
+from Database.Schemas.schemas import Base, MAINTENANCE_CREW, AIRPLANE, STAGE
+from Models.Models import MaintenanceCrewModel, AirplaneModel
 from Authorization.Authorization import KeycloakJWTBearerHandler, HTTPException
 
 import json
@@ -141,6 +141,61 @@ def add_airplane(
     return {"message": "Airplane has been successfully added"}
 
 
+@admin_router.put("/updateAirplane")
+def update_airplane(
+    airplane_data: AirplaneModel,
+    new_airplane_data: AirplaneModel,
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+
+    # Проверяем, что новое id самолета не существует
+    airplane = db.query(AIRPLANE)\
+                 .filter(AIRPLANE.airplane_id == new_airplane_data.id_number)\
+                 .first()
+    if airplane is not None:
+        return JSONResponse(status_code=400, content="Airplane with current id number has already exist")
+    
+    # Проверяем, что тип самолета не пустой
+    if new_airplane_data.type == "":
+        return JSONResponse(status_code=400, content="The type of the airplane is unsatisfactory")
+
+    # Проверяем, что condition добавляемого самолета <= 100 и > 0
+    if new_airplane_data.condition <= 0 or airplane_data.condition > 100:
+        return JSONResponse(status_code=400, content="The condition of the airplane is unsatisfactory")
+    
+    # Проверяем, что обслуживающая бригада назначеная на самолет существет
+    crew = db.query(MAINTENANCE_CREW)\
+             .filter(MAINTENANCE_CREW.maintenance_crew_id == new_airplane_data.maintenance_crew)\
+             .first()
+    if crew is None:
+        return JSONResponse(status_code=400, content="Maintenance crew is not exist")
+    
+    # Проверяем, что новый этап самолета существует
+    stage = db.query(STAGE)\
+              .filter(STAGE.stage_id == new_airplane_data.stage_id)\
+              .first()
+    if stage is None:
+        return JSONResponse(status_code=404, content="Stage is not exist")
+    
+    # Получаем изменяемый самолет
+    airplane = db.query(AIRPLANE)\
+                 .filter(AIRPLANE.airplane_id == airplane_data.id_number)\
+                 .first()
+    # Обновляем данные о самолете
+    airplane.airplane_id = new_airplane_data.id_number
+    airplane.type = new_airplane_data.type
+    airplane.condition = new_airplane_data.condition
+    airplane.maintenance_crew = new_airplane_data.maintenance_crew
+    airplane.stage_id = new_airplane_data.stage_id
+    db.commit()
+
+    return {"message": "Airplane has been successfully added"}
+
+
 @admin_router.delete("/delAirplane")
 def delete_airplane(
     airplane_data: AirplaneModel,
@@ -162,3 +217,156 @@ def delete_airplane(
     db.commit()
 
     return {"message": "Airplane has been successfully deleted"}
+
+
+# ============================== Менеджмент бригад ==============================
+@admin_router.get("/maintenanceCrew")
+def get_all_maintenance_crew(
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+
+    crews = db.query(MAINTENANCE_CREW).all()
+
+    data = dict()
+    data["crews"] = list()
+    for a in crews:
+        d = dict()
+        d["maintenance_crew_id"] = a.maintenance_crew_id
+        d["name"] = a.name
+        data["crews"].append(d)
+
+    return json.loads(json.dumps(data, default=str))
+
+
+@admin_router.post("/maintenanceCrew")
+def get_all_maintenance_crew(
+    mt_crew_data: MaintenanceCrewModel,
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+
+    mt_crew = db.query(MAINTENANCE_CREW)\
+              .filter(MAINTENANCE_CREW.maintenance_crew_id == mt_crew_data.id_number)\
+              .first()
+    if mt_crew is None:
+        return JSONResponse(status_code=404, content="Maintenance crew is not exist")
+
+    # Получаем список всех самолетов
+    airplanes = db.query(AIRPLANE)\
+                  .filter(AIRPLANE.maintenance_crew == mt_crew.maintenance_crew_id)\
+                  .all()
+    
+    data = dict()
+    data["airplanes"] = list()
+    for a in airplanes:
+        d = dict()
+        d["airplane_id"] = a.airplane_id
+        d["type"] = a.type
+        d["condition"] = a.condition
+        d["maintenance_crew"] = a.maintenance_crew
+        d["stage_id"] = a.stage_id
+        data["airplanes"].append(d)
+
+    return json.loads(json.dumps(data, default=str))
+
+
+@admin_router.post("/addMaintenanceCrew")
+def add_maintenance_crew(
+    mt_crew_data: MaintenanceCrewModel,
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+    
+    # Проверяем, что бригады с данным id не существет в БД
+    mt_crew = db.query(MAINTENANCE_CREW)\
+                .filter(MAINTENANCE_CREW.maintenance_crew_id == mt_crew_data.id_number)\
+                .first()
+    if mt_crew is not None:
+        return JSONResponse(status_code=400, content="Maintenance crew with current id is exist")
+    
+    if mt_crew_data.name == "":
+        return JSONResponse(status_code=400, content="Maintenance crew's name is empty")
+    
+    crew = MAINTENANCE_CREW(
+        maintenance_crew_id=mt_crew_data.id_number,
+        name=mt_crew_data.name
+    )
+    db.add(crew)
+    db.commit()
+
+    return {"message": "Maintenance crew has been successfully deleted"}
+
+
+@admin_router.put("/updateMaintenanceCrew")
+def update_maintenance_crew(
+    mt_crew_data: MaintenanceCrewModel,
+    new_mt_crew_data: MaintenanceCrewModel,
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+        
+    # Проверяем, что новый id не существует в БД
+    mt_crew = db.query(MAINTENANCE_CREW)\
+                .filter(MAINTENANCE_CREW.maintenance_crew_id == new_mt_crew_data.id_number)\
+                .first()
+    if mt_crew is not None:
+        return JSONResponse(status_code=400, content="New maintenance crew with current id is exist")
+    
+    # Проверяем, что новое название не пустое
+    if new_mt_crew_data.name == "":
+        return JSONResponse(status_code=400, content="Maintenance crew's name is empty")
+    
+    # Проверяем, что бригады с данным id существет в БД
+    mt_crew = db.query(MAINTENANCE_CREW)\
+                .filter(MAINTENANCE_CREW.maintenance_crew_id == mt_crew_data.id_number)\
+                .first()
+    if mt_crew is None:
+        return JSONResponse(status_code=400, content="Maintenance crew with current id is not exist")
+    
+    if mt_crew_data.name == "":
+        return JSONResponse(status_code=400, content="Maintenance crew's name is empty")
+
+    mt_crew.maintenance_crew_id = new_mt_crew_data.id_number
+    mt_crew.name = new_mt_crew_data.name
+    db.commit()
+
+    return {"message": "Maintenance crew has been successfully deleted"}
+
+
+@admin_router.delete("/deleteMaintenanceCrew")
+def delete_maintenance_crew(
+    mt_crew_data: MaintenanceCrewModel,
+    db: Session = Depends(get_db),
+    role=Depends(KeycloakJWTBearerHandler())
+):
+    # Проверка авторизации
+    if not verify_admin(role):
+        raise HTTPException(status_code=403, detail={"message": "Denied permission"})
+    
+    # Проверяем, что бригады с данным id существет в БД
+    mt_crew = db.query(MAINTENANCE_CREW)\
+                .filter(MAINTENANCE_CREW.maintenance_crew_id == mt_crew_data.id_number)\
+                .first()
+    if mt_crew is not None:
+        return JSONResponse(status_code=400, content="Maintenance crew with current id is exist")
+    
+    if mt_crew_data.name == "":
+        return JSONResponse(status_code=400, content="Maintenance crew's name is empty")
+     
+    db.delete(mt_crew)
+    db.commit()
+
+    return {"message": "Maintenance crew has been successfully deleted"}
